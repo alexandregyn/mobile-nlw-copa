3df73ae3bc7ac67cf291484ca489baf764239b81
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2022-11-02 20:00:29 -0300

    Instalação e configurado dependencia para utilizar SVG

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Center, Text, VStack } from 'native-base';
 
+import Logo from '../assets/logo.svg';
+
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        SignIn
-      </Text>
+      <Logo width={212} height={40}/>
     </Center>
   );
 }
